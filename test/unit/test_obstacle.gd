0c728a22e68e10da468643742dd6a669775bef6b
Justin Hong <dripcoding@gmail.com>
2024-04-24 22:03:33 -0600

    add test

--- test/unit/test_obstacle.gd
+++ test/unit/test_obstacle.gd
@@ -4,4 +4,15 @@ var obstacle_scene = load("res://scenes/obstacle.tscn")
 
 
 func before_each():
-	pass
+	var obstacle = obstacle_scene.instantiate()
+	add_child(obstacle)
+
+
+func after_each():
+	var obstacle = get_node("Obstacle")
+	obstacle.free()
+
+
+func test_obstacle_is_static_body():
+	var obstacle = get_node("Obstacle")
+	assert_true(obstacle is StaticBody2D, "Obstacle should be a StaticBody2D")
