3794325690cb1e50f96fe3a8c25636f8208a3294
Justin Hong <dripcoding@gmail.com>
2024-04-24 21:59:38 -0600

    formatting

--- test/unit/test_obstacle.gd
+++ test/unit/test_obstacle.gd
@@ -1,6 +1,7 @@
-extends 'res://addons/gut/test.gd'
+extends "res://addons/gut/test.gd"
 
 var obstacle_scene = load("res://scenes/obstacle.tscn")
 
+
 func before_each():
-    pass
+	pass
--- test/unit/test_snake_food.gd
+++ test/unit/test_snake_food.gd
@@ -2,13 +2,16 @@ extends "res://addons/gut/test.gd"
 
 var snake_food_scene = load("res://scenes/snake_food.tscn")
 
+
 func before_each():
 	var snake_food = snake_food_scene.instantiate()
 	add_child(snake_food)
 
+
 func after_each():
 	var snake_food = get_node("SnakeFood")
 	snake_food.free()
+
 
 func test_snake_food_is_static_body():
 	var snake_food = get_node("SnakeFood")
