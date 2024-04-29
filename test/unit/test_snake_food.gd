c11d7a67de7a98969b2fd22b3e0b802464e83647
Justin Hong <dripcoding@gmail.com>
2024-04-28 21:30:02 -0600

    add test

--- test/unit/test_snake_food.gd
+++ test/unit/test_snake_food.gd
@@ -15,4 +15,18 @@ func after_each():
 
 func test_snake_food_is_static_body():
 	var snake_food = get_node("SnakeFood")
-	assert_true(snake_food is StaticBody2D, "SnakeFood should be a StaticBody2D")
+	assert_true(snake_food is Area2D, "SnakeFood should be a StaticBody2D")
+
+
+func test_food_eaten_signal_emitted():
+	var snake_food = get_node("SnakeFood")
+	var node = Node2D.new()
+	
+	watch_signals(snake_food)
+
+	snake_food.emit_signal("body_entered", node)
+
+	assert_signal_emitted(snake_food, "food_eaten", "food_eaten signal should be emitted when body enters the area")
+	assert_signal_emit_count(snake_food, "food_eaten", 1, "food_eaten signal should be emitted only once")
+
+	node.free()
